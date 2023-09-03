9aa02d06cbe7d654bf73d71539f6dc4d96e5c585
EnzoJohan <143907383+EnzoJohan@users.noreply.github.com>
2023-09-02 19:11:40 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿
+namespace solucon
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCalcular_Click = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.btnCerrar = new System.Windows.Forms.Button();
+            this.lblProducto = new System.Windows.Forms.Label();
+            this.lblPeso = new System.Windows.Forms.Label();
+            this.lblDestino = new System.Windows.Forms.Label();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.cmbNombreProducto = new System.Windows.Forms.ComboBox();
+            this.cmbPesoKg = new System.Windows.Forms.ComboBox();
+            this.cmbDestino = new System.Windows.Forms.ComboBox();
+            this.txtResultado = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnCalcular_Click
+            // 
+            this.btnCalcular_Click.Location = new System.Drawing.Point(39, 233);
+            this.btnCalcular_Click.Name = "btnCalcular_Click";
+            this.btnCalcular_Click.Size = new System.Drawing.Size(169, 105);
+            this.btnCalcular_Click.TabIndex = 0;
+            this.btnCalcular_Click.Text = "Calcular";
+            this.btnCalcular_Click.UseVisualStyleBackColor = true;
+            this.btnCalcular_Click.Click += new System.EventHandler(this.btnCalcular_Click_Click);
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(336, 382);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(103, 38);
+            this.btnLimpiar.TabIndex = 1;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // btnCerrar
+            // 
+            this.btnCerrar.Location = new System.Drawing.Point(630, 335);
+            this.btnCerrar.Name = "btnCerrar";
+            this.btnCerrar.Size = new System.Drawing.Size(129, 85);
+            this.btnCerrar.TabIndex = 2;
+            this.btnCerrar.Text = "Cerrar";
+            this.btnCerrar.UseVisualStyleBackColor = true;
+            this.btnCerrar.Click += new System.EventHandler(this.btnCerrar_Click);
+            // 
+            // lblProducto
+            // 
+            this.lblProducto.AutoSize = true;
+            this.lblProducto.Location = new System.Drawing.Point(69, 41);
+            this.lblProducto.Name = "lblProducto";
+            this.lblProducto.Size = new System.Drawing.Size(87, 13);
+            this.lblProducto.TabIndex = 3;
+            this.lblProducto.Text = "NombreProducto";
+            // 
+            // lblPeso
+            // 
+            this.lblPeso.AutoSize = true;
+            this.lblPeso.Location = new System.Drawing.Point(258, 41);
+            this.lblPeso.Name = "lblPeso";
+            this.lblPeso.Size = new System.Drawing.Size(31, 13);
+            this.lblPeso.TabIndex = 4;
+            this.lblPeso.Text = "Peso";
+            // 
+            // lblDestino
+            // 
+            this.lblDestino.AutoSize = true;
+            this.lblDestino.Location = new System.Drawing.Point(451, 41);
+            this.lblDestino.Name = "lblDestino";
+            this.lblDestino.Size = new System.Drawing.Size(43, 13);
+            this.lblDestino.TabIndex = 5;
+            this.lblDestino.Text = "Destino";
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Location = new System.Drawing.Point(439, 194);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(55, 13);
+            this.lblResultado.TabIndex = 6;
+            this.lblResultado.Text = "Resultado";
+            // 
+            // cmbNombreProducto
+            // 
+            this.cmbNombreProducto.FormattingEnabled = true;
+            this.cmbNombreProducto.Location = new System.Drawing.Point(55, 75);
+            this.cmbNombreProducto.Name = "cmbNombreProducto";
+            this.cmbNombreProducto.Size = new System.Drawing.Size(121, 21);
+            this.cmbNombreProducto.TabIndex = 7;
+            // 
+            // cmbPesoKg
+            // 
+            this.cmbPesoKg.FormattingEnabled = true;
+            this.cmbPesoKg.Location = new System.Drawing.Point(213, 75);
+            this.cmbPesoKg.Name = "cmbPesoKg";
+            this.cmbPesoKg.Size = new System.Drawing.Size(121, 21);
+            this.cmbPesoKg.TabIndex = 8;
+            // 
+            // cmbDestino
+            // 
+            this.cmbDestino.FormattingEnabled = true;
+            this.cmbDestino.Location = new System.Drawing.Point(408, 75);
+            this.cmbDestino.Name = "cmbDestino";
+            this.cmbDestino.Size = new System.Drawing.Size(121, 21);
+            this.cmbDestino.TabIndex = 9;
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.Location = new System.Drawing.Point(249, 220);
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.Size = new System.Drawing.Size(510, 20);
+            this.txtResultado.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtResultado);
+            this.Controls.Add(this.cmbDestino);
+            this.Controls.Add(this.cmbPesoKg);
+            this.Controls.Add(this.cmbNombreProducto);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.lblDestino);
+            this.Controls.Add(this.lblPeso);
+            this.Controls.Add(this.lblProducto);
+            this.Controls.Add(this.btnCerrar);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.btnCalcular_Click);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalcular_Click;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Button btnCerrar;
+        private System.Windows.Forms.Label lblProducto;
+        private System.Windows.Forms.Label lblPeso;
+        private System.Windows.Forms.Label lblDestino;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.ComboBox cmbNombreProducto;
+        private System.Windows.Forms.ComboBox cmbPesoKg;
+        private System.Windows.Forms.ComboBox cmbDestino;
+        private System.Windows.Forms.TextBox txtResultado;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace solucon
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+            // Configurar opciones para ComboBox de Nombre del Producto
+            cmbNombreProducto.Items.Add("Producto 1");
+            cmbNombreProducto.Items.Add("Producto 2");
+
+            // Configurar opciones para ComboBox de Peso en Kilogramos
+            cmbPesoKg.Items.Add("Menos de 1 kg");
+            cmbPesoKg.Items.Add("1 - 5 kg");
+            cmbPesoKg.Items.Add("Más de 5 kg");
+
+            // Configurar opciones para ComboBox de Destino
+            cmbDestino.Items.Add("San Salvador");
+            cmbDestino.Items.Add("Interior del país");
+        }
+
+        private void btnCalcular_Click_Click(object sender, EventArgs e)
+        {
+            string producto = cmbNombreProducto.SelectedItem.ToString();
+            string peso = cmbPesoKg.SelectedItem.ToString();
+            string destino = cmbDestino.SelectedItem.ToString();
+            decimal costoEnvio = 0;
+
+            // Realizar cálculos según las condiciones dadas
+            if (peso == "Menos de 1 kg")
+            {
+                costoEnvio = 5.00m;
+            }
+            else if (peso == "1 - 5 kg")
+            {
+                costoEnvio = 10.00m;
+            }
+            else if (peso == "Más de 5 kg")
+            {
+                costoEnvio = 10.00m + 2.00m * (decimal)(float.Parse(peso.Split(' ')[0]) - 5);
+            }
+
+            if (destino == "San Salvador" && peso != "Menos de 1 kg")
+            {
+                costoEnvio *= 0.9m; // Aplicar descuento del 10%
+            }
+            else if (destino == "Interior del país")
+            {
+                costoEnvio *= 1.2m; // Aplicar recargo del 20%
+            }
+
+            // Mostrar el resultado en un TextBox o Label
+            txtResultado.Text = $"Producto: {producto}\r\nCosto de Envío: ${costoEnvio:F2}";
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            cmbNombreProducto.SelectedIndex = -1;
+            cmbPesoKg.SelectedIndex = -1;
+            cmbDestino.SelectedIndex = -1;
+            txtResultado.Clear();
+        }
+
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
